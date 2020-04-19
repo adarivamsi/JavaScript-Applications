9d531cdedc7ce31594e04c0c47f74f32bfedb3d9
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-19 19:44:02 -0400

    Added Mobile Booking UI

---
+++ Booking-Seats/Mobile-Booking-UI/script.js
@@ -0,0 +1,70 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const bookSelect = document.getElementById('show');
+
+populateUI();
+
+let ticketPrice = +bookSelect.value;
+
+// Save selected book index and price
+function setbookData(bookIndex, bookPrice) {
+  localStorage.setItem('selectedbookIndex', bookIndex);
+  localStorage.setItem('selectedbookPrice', bookPrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedbookIndex = localStorage.getItem('selectedbookIndex');
+
+  if (selectedbookIndex !== null) {
+    bookSelect.selectedIndex = selectedbookIndex;
+  }
+}
+
+// book select event
+bookSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setbookData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
